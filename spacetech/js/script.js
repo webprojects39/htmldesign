ce74fac00046e23787361b7394c5fd6e73603024
Arda <230775849+webprojects39@users.noreply.github.com>
2025-09-16 21:31:05 +0300

    Add files via upload

---
+++ spacetech/js/script.js
@@ -0,0 +1,40 @@
+/*Name this external file gallery.js*/
+
+function upDate(previewPic){
+ /* In this function you should 
+    1) change the url for the background image of the div with the id = "image" 
+    to the source file of the preview image
+    
+    2) Change the text  of the div with the id = "image" 
+    to the alt text of the preview image 
+    */
+
+    let imageDiv = document.getElementById("image");
+    imageDiv.style.backgroundImage="url('" + previewPic.src + "')";
+    imageDiv.innerHTML=previewPic.alt;
+	}
+
+	function unDo(){
+     /* In this function you should 
+    1) Update the url for the background image of the div with the id = "image" 
+    back to the orginal-image.  You can use the css code to see what that original URL was
+    
+    2) Change the text  of the div with the id = "image" 
+    back to the original text.  You can use the html code to see what that original text was
+    */
+
+    let imageDiv = document.getElementById("image");
+    imageDiv.style.backgroundImage = "none";
+    imageDiv.style.backgroundColor = "#8e68ff";
+    imageDiv.innerHTML = "Hover over an image below to display here.";		
+	}
+
+    function addTabindex(){
+        const images = document.getElementsByTagName("img");
+        for (let index = 0; index < images.length; index++) {
+            const element = images[index];
+            element.setAttribute("tabindex", "0");
+            console.log("Tabindex added to image: " + images[index].alt)  
+        }
+
+    }
